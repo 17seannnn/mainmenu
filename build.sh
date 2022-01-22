46fa631d8517c4624abad5e49cea715b6f77dbbe
17seannnn <17sean@protonmail.com>
2022-01-22 16:04:35 +0300

    build: update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
-cd src
-gcc -Wall -Og -g -c -lncursesw mainmenu.c
-gcc -Wall -Og -g -lncursesw test.c mainmenu.o -o ../test
+
+case $1 in
+        install)
+                cd src
+                gcc -Wall -Og -g -c -lncursesw mainmenu.c
+                gcc -Wall -Og -g -lncursesw test.c mainmenu.o -o ../test
+		cd ..
+                msgfmt po/ru.po -o /usr/share/locale/ru/LC_MESSAGES/mmtest.mo;;
+        pot) xgettext --keyword="_" -f po/POTFILES.in -D src -o po/mmtest.pot;;
+        * | help) echo "\
+Usage: build.sh [CMD]
+Commands
+        install   build test program
+        pot       generate pot file
+        help      show this help
+";;
+esac
