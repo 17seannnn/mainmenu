dfa00b0f9528bb5c4de37d1cceb96d311c795b3d
17seannnn <17sean@protonmail.com>
2022-01-26 20:04:32 +0300

    build.sh: add ansi option for gcc

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 case $1 in
         install)
                 cd src
-                gcc -Wall -Og -g -c -lncursesw mainmenu.c
-                gcc -Wall -Og -g -lncursesw test.c mainmenu.o -o ../test
+                gcc -ansi -pedantic -Wall -Og -g -c -lncursesw mainmenu.c
+                gcc -ansi -pedantic -Wall -Og -g -lncursesw test.c mainmenu.o -o ../test
 		cd ..
                 msgfmt po/ru.po -o /usr/share/locale/ru/LC_MESSAGES/mmtest.mo;;
         pot) xgettext --keyword="_" -f po/POTFILES.in -D src -o po/mmtest.pot;;
